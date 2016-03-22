f05ebc9e349083d03537e538876c579594ea0ef0
sidious18 <max_leb@mail.ru>
2016-03-22 12:19:13 +0200

    facebook changes

--- js/hamscripts.js
+++ js/hamscripts.js
@@ -14,3 +14,22 @@ $( window ).resize(function() {
 		$(".right-season-gallery img").css("height","auto");
 	}
 });
+
+//init facebook
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1533695430259772',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+};
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+//init twitter
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
