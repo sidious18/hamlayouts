53d21058fb5a1d80ddf9d5b3342c60cd137b6a46
sidious18 <max_leb@mail.ru>
2016-03-28 13:48:58 +0300

    Contact us page

--- js/hamscripts.js
+++ js/hamscripts.js
@@ -1,7 +1,4 @@
 jQuery(document).ready(function($) {
-	$(".subscribe-button").click(function(event) {
-		setTimeout('$("#subscribe-modal").modal("hide")', 3000)
-	});
 	if($(document).outerWidth() <= 768){
 		$(".left-season-link img").css("height",$(".left-season-link img").css("width"));
 		$(".right-season-gallery img").css("height",$(".right-season-gallery img").css("width"));
@@ -36,3 +33,21 @@ window.fbAsyncInit = function() {
 
 //init twitter
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+//init google-maps
+var hamCenter=new google.maps.LatLng(35.427433,-77.662727);
+function initialize() {
+	  var mapProp = {
+	    center:new google.maps.LatLng(35.416557,-77.688734),
+	    zoom:10,
+	    mapTypeId:google.maps.MapTypeId.ROADMAP,
+	    scrollwheel: false
+	  };
+	  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	  var marker=new google.maps.Marker({
+	  	position:hamCenter
+	  });
+
+	marker.setMap(map);
+	}
+	google.maps.event.addDomListener(window, 'load', initialize);
